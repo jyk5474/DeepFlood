34b3523e3325cfb01c6becf0b5ab2af612b770b8
JackKociolek <97631132+jyk5474@users.noreply.github.com>
2024-04-17 22:07:09 -0400

    Update MobileNet.py

--- MobileNet.py
+++ MobileNet.py
@@ -3,8 +3,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications import MobileNetV2
 
-
-# Load the mean images and labels from the saved files
 mean_images = np.load(r'C:\Users\Jekoc\Desktop\DS340W\DS\mean_images.npy')
 labels = np.load(r'C:\Users\Jekoc\Desktop\DS340W\DS\labels.npy')
 
@@ -15,20 +13,17 @@ X_train, X_test, y_train, y_test = train_test_split(mean_images, labels, test_si
 
 base_model = MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
 
-# Extract features using the pre-trained MobileNetV2 model
+# extract features using the pre-trained MobileNetV2 model
 train_features = base_model.predict(X_train)
 test_features = base_model.predict(X_test)
 
-
-# Reshape the features to flatten them
 train_features_flat = X_train.reshape(X_train.shape[0], -1)
 test_features_flat = X_test.reshape(X_test.shape[0], -1)
 
-# Train a Random Forest classifier
 clf = RandomForestClassifier()
 clf.fit(train_features_flat, y_train)
 
-# Predict labels using the trained classifier
+# Predict
 y_pred_train = clf.predict(train_features_flat)
 y_pred_test = clf.predict(test_features_flat)
 
